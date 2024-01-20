dc9e1e575b4dc4466254a3486e9a97d2f57275e3
Kawish-Raj <rajkawish@gmail.com>
2024-01-20 22:08:23 +1100

    added colorful hover effect

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ for(let i=0; i < gridLen; i++){
     for (let j = 0; j < gridLen; j++) {
         const gridDiv = document.createElement('div');
         gridDiv.classList.add('grid_div');
+
+        gridDiv.addEventListener('mouseover', () => {
+
+            let rVal = getRandomPixel();
+            let gVal = getRandomPixel();
+            let bVal = getRandomPixel();
+            gridDiv.style.cssText = `background-color: rgb(${rVal},${gVal},${bVal});`;
+            
+            //black pen
+            //gridDiv.style.cssText = "background-color: black;"
+        
+        })
+
         rowDiv.appendChild(gridDiv);
     }
+}
+
+function getRandomPixel(){
+    return Math.floor(Math.random() * 255);
 }
