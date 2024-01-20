beae5ae0ce951c4b1be24f338c44b5491ff73eca
Kawish-Raj <rajkawish@gmail.com>
2024-01-20 22:26:45 +1100

    user can change grid size now

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
 const gridContainerDiv = document.querySelector(".grid_container");
+const sizeButton = document.querySelector(".size-button");
 
-let gridLen = 16;
+let gridSize = 16;
 
-for(let i=0; i < gridLen; i++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row_div');
-    gridContainerDiv.appendChild(rowDiv);
-    for (let j = 0; j < gridLen; j++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.classList.add('grid_div');
+function genrateGrid() {
 
-        gridDiv.addEventListener('mouseover', () => {
+    for (let i = 0; i < gridSize; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row_div');
+        gridContainerDiv.appendChild(rowDiv);
+        for (let j = 0; j < gridSize; j++) {
+            const gridDiv = document.createElement('div');
+            gridDiv.classList.add('grid_div');
 
-            let rVal = getRandomPixel();
-            let gVal = getRandomPixel();
-            let bVal = getRandomPixel();
-            gridDiv.style.cssText = `background-color: rgb(${rVal},${gVal},${bVal});`;
-            
-            //black pen
-            //gridDiv.style.cssText = "background-color: black;"
-        
-        })
+            gridDiv.addEventListener('mouseover', () => {
 
-        rowDiv.appendChild(gridDiv);
+                let rVal = getRandomPixel();
+                let gVal = getRandomPixel();
+                let bVal = getRandomPixel();
+                gridDiv.style.cssText = `background-color: rgb(${rVal},${gVal},${bVal});`;
+
+                //black pen
+                //gridDiv.style.cssText = "background-color: black;"
+
+            })
+
+            rowDiv.appendChild(gridDiv);
+        }
+    }
+
+}
+
+genrateGrid();
+
+function emptyGrid(){
+    while (gridContainerDiv.firstChild){
+        gridContainerDiv.removeChild(gridContainerDiv.firstChild);
     }
 }
 
 function getRandomPixel(){
     return Math.floor(Math.random() * 255);
 }
+
+sizeButton.addEventListener('click',() => {
+    let tempGridSize = prompt("Enter number between 16 and 100 to set grid size");
+    
+    if (tempGridSize >= 16 && tempGridSize <= 100) {
+        emptyGrid();
+        gridSize = tempGridSize;
+        genrateGrid();
+    }
+    else{
+        alert("Please enter a value between 16 and 100");
+    }
+})
